b4cc9d752270d611ab143b870c5156982cd5b7b2
Gerardo Mundo <ger.mundo66@gmail.com>
2023-10-11 16:18:09 -0600

    Initial commit

---
+++ FlexorTest/Controllers/Products.cs
@@ -0,0 +1,43 @@
+﻿using FlexorTest.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlexorTest.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // POST api/products
+        [HttpPost]
+        public ActionResult Post([FromBody] List<Product> products)
+        {
+            if(products.Count == 0 || products == null)
+            {
+                return BadRequest("No se agregaron productos.");
+            }
+
+            var subTotal = 0;
+            decimal discount = 0;
+
+
+            foreach (var product in products)
+            {
+                subTotal += product.Price * product.Quantity;
+                discount += product.Discount;
+            }
+
+            decimal iva = (subTotal * 13) / 100;
+            decimal total = (subTotal + iva) - discount;
+
+            var response = new
+            {
+                Subtotal = subTotal,
+                Iva = iva,
+                Descuento = discount,
+                Total = total,
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ FlexorTest/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace FlexorTest.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public int Discount { get; set; }
+    }
+}
